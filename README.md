97dab970f0694aea58b264f70981e4a33c17fd18
DiGiV3rse <147015881+digiv3rse@users.noreply.github.com>
2024-03-28 23:32:06 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,84 @@
+# On-chain asset Store
+
+On-chain asset store, which allows multiple smart contracts to share vector assets.
+
+All assets registered to this store will be treated as cc0 (public domain),
+CC-BY(attribution), Apache 2.0 or MIT (should be specified in the "group").
+In case of CC-BY, the creater's name should be in the "group", "category" or "name".
+
+All registered assets will be available to other smart contracts for free, including
+commercial services. Therefore, it is not allowed to register assets that require
+any form of commercial licenses.
+
+Once an asset is registered with group/category/name, it is NOT possible to update,
+which guarantees the availability in future.
+
+Created by Satoshi Nakajima (@snakajima)
+
+## WebUI repository
+
+The corresponding WebUI repository assumes that it can access this repository via "../contract" to access some generated contents in the "cache" folder. 
+
+## Setup package
+
+```
+yarn install
+```
+
+## Setup your wallet
+
+### setup .env for sepolia
+
+#### Web3 Provider
+
+Get Api key from Web3 Provider and set API KEY
+
+```
+INFURA_API_KEY = "xxxxx"
+```
+
+or
+
+```
+ALCHEMY_API_KEY = "xxxx"
+```
+
+#### Your Account
+
+Set your account
+
+
+```
+MNEMONIC = "hoge hoge hoge"
+ACCOUNT_INITIAL_INDEX = 2
+```
+
+or 
+
+```
+PRIVATE_KEY= "hogehoge"
+```
+
+### deploy to sepolia
+
+```
+npx hardhat --network sepolia run scripts/deploy-sepolia.ts 
+```
+
+## deploy to local
+
+```
+npx hardhat --network localhost run scripts/deploy.ts 
+```
+
+# Etherscan verification
+
+```
+npx hardhat verify --network mainnet --constructor-args arguments.js CONTRACT_ADDRESS
+```
+
+# local unit test
+
+```
+npx hardhat test --network hardhat
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,95 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const getUrl = () => {
+  return process.env.INFURA_API_KEY ?
+    "https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY : 
+    "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY
+};
+const getAccount = () => {
+  return process.env.MNEMONIC ? {
+    initialIndex: process.env.ACCOUNT_INITIAL_INDEX ? Number(process.env.ACCOUNT_INITIAL_INDEX) : 0,
+    mnemonic: process.env.MNEMONIC,
+  } : (process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [])
+};
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1_000,
+      },
+    },
+  },
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      timeout: 10000000,
+    },
+    hardhat: {
+      chainId: 1337,
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY,
+      }
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    sepolia: {
+      url: getUrl(),
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    mainnet: {
+      url: "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY,
+      gasMultiplier: 1.5,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    matic: {
+      url: "https://polygon-mainnet.g.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY,
+      gasMultiplier: 1.5,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
